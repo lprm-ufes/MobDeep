763bffe0f149c37635c9838df22612e6a2ea646e
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2024-04-27 17:47:02 +0200

    training using timegan

--- models/timegan/helper.py
+++ models/timegan/helper.py
@@ -8,18 +8,17 @@ def parse_arguments():
     parse.add_argument('-m', '--rnn_model', help='train rnn model on the fly', action='store_true', default=False)    
 
     # model hyperparameters
-    # parse.add_argument('-n', '--num_steps', help='list of num_steps', nargs='+', type=int)
-    # parse.add_argument('-hd','--hidden_dimensions', help='list of hidden_dimensios', nargs='+', type=int)
-    # parse.add_argument('-nd','--noise_dimensions', help='list of noise_dimensios', nargs='+', type=int)
-    # parse.add_argument('-dm','--dimensions',help='list of dims', nargs='+', type=int)
-    # parse.add_argument('-l', '--learning_rates', help='list of learning_rates', nargs='+', type=float)
-    # parse.add_argument('-bs','--batch_sizes', help='list of batch_sizes', nargs='+', type=int)
-    
-    # Weights&Biases
-    parse.add_argument('-w', '--connect_wandb', action='store_true', help='connect to weights&biases dashboard')
-    parse.add_argument('-p', '--project_name', type=str, help='set project name')
-    parse.add_argument('-e', '--exp_name', type=str, help='set the experiment name')
-    parse.add_argument('-r', '--runs', type=int, nargs='+', required=True, help='Experiments number')
+    parse.add_argument('-n', '--num_steps', help='num_steps', type=int)
+    parse.add_argument('-hd','--hidden_dimension', help='hidden_dimension', type=int)
+    parse.add_argument('-nd','--noise_dimension', help='noise_dimensio', type=int)
+    parse.add_argument('-dm','--dimension', help='dims', type=int)
+    parse.add_argument('-l', '--learning_rate', help='learning_rate', type=float)
+    parse.add_argument('-bs','--batch_size', help='batch_size', type=int)
+
+    #
+    parse.add_argument('-ti', '--test_id', help='Test id')
+    parse.add_argument('-ei', '--exp_id', help='Exp id')
+    parse.add_argument('-s', '--scale', action='store_true', help='Whether to scale the data before training (not necessarry if the data is already scaled.)')
 
     args = parse.parse_args()
     return args
--- models/timegan/train_model.py
+++ models/timegan/train_model.py
@@ -14,10 +14,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 import helper
 import metrics as m
-import utils as u
+import timegan_utils as u
 from timegan.model import TimeGAN
 
-def train(X, num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes, project_nm, exp_nm, runs=[], scaler=None, timestamps=[], connect_wb=False):
+def train(X, num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes, exp_id=None, test_id=None):
     """
     Train TimeGAN model using parameters passed by command line
 
@@ -35,27 +35,20 @@ def train(X, num_steps, hidden_dimensions, noise_dimensions, dimensions, learnin
     """
     shp = X.shape
     col0 = X.reshape(shp[0]*shp[1], shp[-1])[:, 0]
-    c = 0
-    for nsteps, h, nd, d, lr, bs in zip(num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes):
-        # nsteps, h, nd, d, lr, bs = params
-        run = runs[c]
-        tf.keras.backend.clear_session()
-        # rnn = u.train_rnn(X, X.shape[1])
-        # u1 = u.print_memory_usage()
-        gan_args_m1, seq_len_m1, n_seq_m1, hidden_dim_m1, gamma_m1, log_step_m1  = u.set_params(X, h, 1, nd, d, bs, 100, lr)
-        # training
-        synth = TimeGAN(model_parameters=gan_args_m1, hidden_dim=hidden_dim_m1, seq_len=seq_len_m1, n_seq=n_seq_m1, gamma=gamma_m1)
-        u.creat_project_dir(project_nm, run)
-        synth.train(X, train_steps=nsteps)
-        synth.save(f"trainings/{project_nm}/{run}/model.pkl")
-        title = f"hidden_dim:{h}|dim:{d}|lr:{lr}|noise_dim:{nd}|bs:{bs}|nsteps:{nsteps}|pn:{project_nm}|run:{run}"
-        figname = u.get_figname(run)  
-        run+=1
-        print ("Pausing execution for 10s")
-        gc.collect()
-        del synth                            
-        time.sleep(10)
-        c+=1
+    nsteps, h, nd, d, lr, bs = [num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes]
+    # nsteps, h, nd, d, lr, bs = params
+    tf.keras.backend.clear_session()
+    # rnn = u.train_rnn(X, X.shape[1])
+    # u1 = u.print_memory_usage()
+    gan_args_m1, seq_len_m1, n_seq_m1, hidden_dim_m1, gamma_m1, log_step_m1  = u.set_params(X, h, 1, nd, d, bs, 100, lr)
+    # training
+    synth = TimeGAN(model_parameters=gan_args_m1, hidden_dim=hidden_dim_m1, seq_len=seq_len_m1, n_seq=n_seq_m1, gamma=gamma_m1)
+    synth.train(X, train_steps=nsteps)
+    synth.save(f"experiments/{exp_id}/{test_id}/models/model.pkl")  
+    print ("Pausing execution for 10s")
+    gc.collect()
+    del synth                            
+    time.sleep(10)
 
 def get_params(runs=[]):
     columns = ["num_steps", "hidden_dimensions", "noise_dimensions", "dimensions", "learning_rates", "batch_sizes"]
@@ -65,10 +58,13 @@ def get_params(runs=[]):
 
 if __name__=='__main__':
     args = helper.parse_arguments()
-    X, scaler = u.get_dataset(args.dataset, args.timesteps)
+    X, scaler = u.get_dataset(args.dataset, args.timesteps, args.scale)
     timestamps = u.get_timestamps(args.timestamps)
+    num_steps = args.num_steps
+    hidden_dimensions = args.hidden_dimension    
+    noise_dimensions = args.noise_dimension
+    dimensions = args.dimension
+    learning_rates = args.learning_rate
+    batch_sizes = args.batch_size
 
-    num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes = get_params(args.runs)
-
-    train(X, num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes, project_nm=args.project_name, exp_nm=args.exp_name,
-        runs=args.runs, scaler=scaler, timestamps=timestamps, connect_wb=args.connect_wandb)
+    train(X, num_steps, hidden_dimensions, noise_dimensions, dimensions, learning_rates, batch_sizes, exp_id=args.exp_id, test_id=args.test_id)
